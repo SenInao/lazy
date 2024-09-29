211dd51a8507732dac1b1ab731be945a193cd38a
SenInao <senharald07@gmail.com>
2024-09-29 23:26:40 +0200

    Added harpoon keybinds

--- init.vim
+++ init.vim
@@ -1,1 +1,2 @@
+set termguicolors
 lua require("init")
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -22,12 +22,20 @@ return {
     vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
     vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
     vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
-    vim.keymap.set("n", "<C-i>", function() harpoon:list():select(5) end)
+
+    vim.keymap.set("n", "<M-h>", function() harpoon:list():select(5) end)
+    vim.keymap.set("n", "<M-j>", function() harpoon:list():select(6) end)
+    vim.keymap.set("n", "<M-k>", function() harpoon:list():select(7) end)
+    vim.keymap.set("n", "<M-l>", function() harpoon:list():select(8) end)
 
     vim.keymap.set("n", "<leader><C-h>", function () harpoon:list():replace_at(1) end)
     vim.keymap.set("n", "<leader><C-j>", function () harpoon:list():replace_at(2) end)
     vim.keymap.set("n", "<leader><C-k>", function () harpoon:list():replace_at(3) end)
     vim.keymap.set("n", "<leader><C-l>", function () harpoon:list():replace_at(4) end)
-    vim.keymap.set("n", "<leader><C-i>", function () harpoon:list():replace_at(5) end)
+
+    vim.keymap.set("n", "<leader><M-h>", function () harpoon:list():replace_at(5) end)
+    vim.keymap.set("n", "<leader><M-j>", function () harpoon:list():replace_at(6) end)
+    vim.keymap.set("n", "<leader><M-k>", function () harpoon:list():replace_at(7) end)
+    vim.keymap.set("n", "<leader><M-l>", function () harpoon:list():replace_at(8) end)
   end
 }
